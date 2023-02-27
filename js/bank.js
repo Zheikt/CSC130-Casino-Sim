809c8633012ebae3ad08426092d547d02704d209
Samuel Howsden <sjdhowsden@gmail.com>
2023-02-27 10:37:42 -0700

    Added function to change num value to monetary string. Added imgTransferDirection logic.

--- js/bank.js
+++ js/bank.js
@@ -13,19 +13,28 @@ let playerChipsLS = localStorage.getItem("chipsBalance");
 playerBankLS = playerBankLS === null ? 10000 : parseInt(playerBankLS);
 playerChipsLS = playerChipsLS === null ? 0 : parseInt(playerChipsLS);
 
+imgTransferDirection.style.visibility = 'hidden';
+
 updateLabels();
 
 btnBack.onclick = (ev) => {
-    window.location.assign('./index.html');
+    localStorage.setItem("bankBalance", playerBankLS);
+    localStorage.setItem("chipsBalance", playerChipsLS);
+    window.location.assign('./MainMenu.html');
 }
 
 btnDeposit.onclick = (ev) => {
     let depositAmount = parseInt(txtChipTransfer.value);
 
-    playerChipsLS -= depositAmount;
-    playerBankLS += depositAmount;
+    if(depositAmount <= playerChipsLS){
+        playerChipsLS -= depositAmount;
+        playerBankLS += depositAmount;
 
-    updateLabels();
+        updateLabels();
+        imgTransferDirection.style.visibility = 'hidden';
+    } else {
+
+    }
 }
 
 btnWithdraw.onclick = (ev) => {
@@ -35,14 +44,19 @@ btnWithdraw.onclick = (ev) => {
     playerBankLS -= withdrawAmount;
 
     updateLabels();
+    imgTransferDirection.style.visibility = 'hidden';
 }
 
 txtChipTransfer.oninput = (ev) => {
     enableTransferButton(txtChipTransfer, btnDeposit);
+    imgTransferDirection.style.transform = 'rotate(-90deg)';
+    imgTransferDirection.style.visibility = 'visible';
 }
 
 txtBankTransfer.oninput = (ev) => {
     enableTransferButton(txtBankTransfer, btnWithdraw);
+    imgTransferDirection.style.transform = 'rotate(90deg)';
+    imgTransferDirection.style.visibility = 'visible';
 }
 
 function enableTransferButton(txtTransfer, btnTransfer){
@@ -55,6 +69,27 @@ function enableTransferButton(txtTransfer, btnTransfer){
 }
 
 function updateLabels(){
-    lblAmountBank.innerText = playerBankLS;
-    lblAmountChips.innerText = playerChipsLS;
+    if(playerBankLS < 0){
+        lblAmountBank.style.color = 'red';
+    } else {
+        
+        lblAmountBank.style.color= 'white';
+    }
+    lblAmountBank.innerText = NumToMoneyStr(playerBankLS);
+    lblAmountChips.innerText = NumToMoneyStr(playerChipsLS);
+
+    localStorage.setItem("bankBalance", playerBankLS);
+    localStorage.setItem("chipsBalance", playerChipsLS);
+}
+
+function NumToMoneyStr(iVal){
+    let bIsNegative = iVal < 0;
+    let strVal = "" + Math.abs(iVal);
+    let numOfCommas = Math.floor(Math.pow(iVal, (1.0/1000.0))); //equivalent to doing a 1000-th root
+    for(let iter = strVal.length - 3; iter > 0; iter -= 3){
+        strVal = strVal.slice(0, iter) + ',' + strVal.slice(iter);
+    }
+
+    strVal = (bIsNegative ? '-' : '') + '$' + strVal;
+    return strVal;
 }
